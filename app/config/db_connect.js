96ad758838b30fe9465e86d83ee0eab9b22457ad
huonguy <uthuong125@gmail.com>
2022-07-27 14:14:07 +0700

    fixed - changed db config

--- app/config/db_connect.js
+++ app/config/db_connect.js
@@ -1,12 +1,23 @@
 const { Sequelize } = require("sequelize");
 
+// const sequelize = new Sequelize(
+//   "db_AirBnB", //database name
+//   "root", //username
+//   "admin123", //password
+//   {
+//     host: "127.0.0.1", //url sql server, RDMS
+//     port: "3308", //port
+//     dialect: "mysql", //sql server dang su dung
+//   }
+// );
+
 const sequelize = new Sequelize(
-  "db_AirBnB", //database name
-  "root", //username
-  "admin123", //password
+  "heroku_652930bd7ed8003", //database name
+  "b07acb687d3809", //username
+  "e3ca9b30", //password
   {
-    host: "127.0.0.1", //url sql server, RDMS
-    port: "3308", //port
+    host: "us-cdbr-east-06.cleardb.net", //url sql server, RDMS
+    port: "3306", //port
     dialect: "mysql", //sql server dang su dung
   }
 );
