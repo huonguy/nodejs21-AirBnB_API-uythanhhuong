c359b3e6bc56cd2c1523e43d74060947217cd1dd
huonguy <uthuong125@gmail.com>
2022-07-27 10:57:05 +0700

    fixed - change port + script

--- app/server.js
+++ app/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const rootRouter = require("./routers/root.router");
-const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 require("dotenv").config(__dirname);
 
 //chuyển req, res về json để tiện thao tác
