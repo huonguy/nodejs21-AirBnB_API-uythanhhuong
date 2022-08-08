765c1c812ff7d80e43deb58aa2eac7cd10898e45
huonguy <uthuong125@gmail.com>
2022-08-08 11:24:29 +0700

    update changes - dbconnect

--- app/config/db_connect.js
+++ app/config/db_connect.js
@@ -1,26 +1,26 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "db_AirBnB", //database name
-  "root", //username
-  "admin123", //password
-  {
-    host: "127.0.0.1", //url sql server, RDMS
-    port: "3308", //port
-    dialect: "mysql", //sql server dang su dung
-  }
-);
-
 // const sequelize = new Sequelize(
-//   "heroku_652930bd7ed8003", //database name
-//   "b07acb687d3809", //username
-//   "e3ca9b30", //password
+//   "db_AirBnB", //database name
+//   "root", //username
+//   "admin123", //password
 //   {
-//     host: "us-cdbr-east-06.cleardb.net", //url sql server, RDMS
-//     port: "3306", //port
+//     host: "127.0.0.1", //url sql server, RDMS
+//     port: "3308", //port
 //     dialect: "mysql", //sql server dang su dung
 //   }
 // );
+
+const sequelize = new Sequelize(
+  "heroku_0ee24c01174b19b", //database name
+  "bc8760a4db03f9", //username
+  "b12b1f25", //password
+  {
+    host: "us-cdbr-east-06.cleardb.net", //url sql server, RDMS
+    port: "3306", //port
+    dialect: "mysql", //sql server dang su dung
+  }
+);
 
 const checkConnect = async () => {
   try {
